ef090b6dec50865eaf35d6b084b71cda8254a81a
Brayan Hernandez <bs.hernandezb1@uniandes.edu.co>
2022-04-30 18:03:39 -0500

    Se agrega servicio que obtiene plantas y componente lista-plantas

---
+++ src/app/plantas/lista-plantas/lista-plantas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-plantas',
+  templateUrl: './lista-plantas.component.html',
+  styleUrls: ['./lista-plantas.component.css']
+})
+export class ListaPlantasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/plantas/plantas.module.ts
+++ src/app/plantas/plantas.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListaPlantasComponent } from './lista-plantas/lista-plantas.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  exports:[ListaPlantasComponent],
+  declarations: [ListaPlantasComponent]
 })
 export class PlantasModule { }
