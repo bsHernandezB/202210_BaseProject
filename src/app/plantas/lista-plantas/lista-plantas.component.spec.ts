95c745adb0bdd3e47900aaaff5a5f169963e4e46
Brayan Hernandez <bs.hernandezb1@uniandes.edu.co>
2022-04-30 19:42:50 -0500

    Se realizo la prueba al componente agregando tres plantas y verificando las filas generadas

--- src/app/plantas/lista-plantas/lista-plantas.component.spec.ts
+++ src/app/plantas/lista-plantas/lista-plantas.component.spec.ts
@@ -2,16 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
 
 import { ListaPlantasComponent } from './lista-plantas.component';
+import { PlantaService } from '../planta.service';
+import { Planta } from '../planta';
 
 describe('ListaPlantasComponent', () => {
   let component: ListaPlantasComponent;
   let fixture: ComponentFixture<ListaPlantasComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListaPlantasComponent ]
+      imports: [HttpClientModule],
+      declarations: [ ListaPlantasComponent ],
+      providers:[PlantaService]
     })
     .compileComponents();
   }));
@@ -19,10 +26,48 @@ describe('ListaPlantasComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListaPlantasComponent);
     component = fixture.componentInstance;
+
+    component.plantas = [
+      new Planta(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      ),
+      new Planta(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      ),
+      new Planta(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      )
+    ];
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Prueba encabezado nombre_cientifico ', () => {
+    let trs = fixture.nativeElement.querySelectorAll('tr');
+
+    expect(trs.length).toBe(4);
   });
 });
